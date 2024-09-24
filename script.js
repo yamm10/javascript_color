cec2e2462fced2b99d7b7808a53d993b33f34027
yamm10 <131327547+yamm10@users.noreply.github.com>
2024-09-24 10:39:10 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// JavaScript
+//console.log(document.getElementById('myColor').value);
+const color = document.getElementById('myColor');
+const text = document.getElementById('bodyText');
+
+text.textContent = color.value;
+
+function changeColor() {
+    // カラーコードを表示
+    text.textContent = 'コード：' + color.value;
+    if (color.value === '#ffffff') {
+        text.textContent = 'コード：' + color.value + '(white)';
+    } else if (color.value === '#000000') {
+        text.textContent = 'コード：' + color.value + '(black)';
+    } else {
+        text.textContent = 'コード：' + color.value;
+    }
+
+    // 背景色を変更
+    document.body.style.backgroundColor = color.value;
+}
+
+// カラーピッカーで色が選択されたら、changeColorを動作させる
+color.addEventListener('input', changeColor);
+
